14e2bf2338efce5a17dbd4d9a1538fc74ce1a853
sadikgell <sadikgell@gmail.com>
2024-01-26 23:46:08 +0300

    Interact script

--- BabyGame/Assets/Script/Interaction.cs
+++ BabyGame/Assets/Script/Interaction.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngineInternal;
 
 public class Interaction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image crosshair;
+    public float distance;
+    public bool isInteract = false;
+    private GameObject interactableObject;
+    public float maxDistanceFromPlayer = 0.5f;
+    // Update is called once per frame
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, forward, out hit))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+            
+            if(hit.distance <= distance && hit.collider.gameObject.tag == "Interact")
+            {
+                interactableObject = hit.collider.gameObject;
+
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    isInteract = !isInteract;
+                }
+            }
+        }
+        if (isInteract && interactableObject != null)
+        {
+            Vector3 targetPosition = transform.position + forward * maxDistanceFromPlayer;
+            interactableObject.transform.position = Vector3.Lerp(
+                interactableObject.transform.position, targetPosition, Time.deltaTime * 10f);
+        }
+
     }
 }
