ad883c14cc52aeda6c0d687b9273d3f4510544c8
mustafayngl <95026273+mustafayngl@users.noreply.github.com>
2024-01-27 03:38:57 +0300

    interaction update
    v2

--- BabyGame/Assets/Script/Interaction.cs
+++ BabyGame/Assets/Script/Interaction.cs
@@ -12,6 +12,7 @@ public class Interaction : MonoBehaviour
     private Vector3 initialObjectPosition;
     public float maxDistanceFromPlayer = 0.5f;
     public float dropDistanceThreshold = 1.5f;
+    public float maxDropDistance = 5f;
 
     void Update()
     {
@@ -24,13 +25,18 @@ public class Interaction : MonoBehaviour
 
             if (hit.distance <= distance && (hit.collider.gameObject.tag == "InteractNeg" || hit.collider.gameObject.tag == "InteractPos"))
             {
-                if (Input.GetKey(KeyCode.F))
+                if (Input.GetKeyDown(KeyCode.F))
                 {
                     if (!isInteract)
                     {
                         interactableObject = hit.collider.gameObject;
                         initialObjectPosition = interactableObject.transform.position;
                         isInteract = true;
+                    }
+                    else
+                    {
+                        interactableObject = null;
+                        isInteract = false;
                     }
                 }
             }
@@ -43,11 +49,14 @@ public class Interaction : MonoBehaviour
                 interactableObject.transform.position, targetPosition, Time.deltaTime * 10f);
         }
 
+        // Check if the object is within the drop distance threshold
         if (!Input.GetKey(KeyCode.F) && isInteract && interactableObject != null)
         {
             float distanceToInitialPosition = Vector3.Distance(interactableObject.transform.position, initialObjectPosition);
+            float playerToInitialPositionDistance = Vector3.Distance(transform.position, initialObjectPosition);
 
-            if (distanceToInitialPosition <= dropDistanceThreshold)
+
+            if (distanceToInitialPosition <= dropDistanceThreshold && playerToInitialPositionDistance <= maxDropDistance)
             {
                 interactableObject.transform.position = initialObjectPosition;
                 isInteract = false;
