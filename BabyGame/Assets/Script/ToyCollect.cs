848a53441224227ddabe5a6688485451a5d8f297
sadikgell <sadikgell@gmail.com>
2024-01-28 00:50:42 +0300

    TOYCOLLECT DONE GİBİ

--- BabyGame/Assets/Script/ToyCollect.cs
+++ BabyGame/Assets/Script/ToyCollect.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class ToyCollect : MonoBehaviour
@@ -21,16 +22,33 @@ public class ToyCollect : MonoBehaviour
     public GameObject toy4;
     public Transform toy4Area;
 
+    public GameObject toy5;
+    public Transform toy5Area;
+
+    public GameObject toy6;
+    public Transform toy6Area;
+
+    public GameObject toy7;
+    public Transform toy7Area;
+
+    public GameObject toy8;
+    public Transform toy8Area;
+
+
     // Start is called before the first frame update
     void Start()
     {
         interaciton = GameObject.Find("Main Camera").GetComponent<Interaction>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
-        AreaCreator(toy1, toy1Area);
-        AreaCreator(toy2, toy2Area);
-        AreaCreator(toy3, toy3Area);
-        AreaCreator(toy4, toy4Area);
+        toy1Area = AreaCreator(toy1,"Toy1Area");
+        toy2Area = AreaCreator(toy2, "Toy2Area");
+        toy3Area = AreaCreator(toy3, "Toy3Area");
+        toy4Area = AreaCreator(toy4, "Toy4Area");
+        toy5Area = AreaCreator(toy5, "Toy5Area");
+        toy6Area = AreaCreator(toy6, "Toy6Area");
+        toy7Area = AreaCreator(toy7, "Toy7Area");
+        toy8Area = AreaCreator(toy8, "Toy8Area");
 
     }   
 
@@ -40,25 +58,57 @@ public class ToyCollect : MonoBehaviour
         if (other.gameObject.transform.parent.name == "Toys")
         {
 
-            if (ToyNumber == 0) 
+            switch  (ToyNumber)
             {
-                ToyTP(other, toy1Area);
+                case 1:
+                    ToyTP(other, toy1Area);
 
-                interaciton.InteractionClear();
-                ToyNumber++;
-                
-            }
-            if (ToyNumber == 1)
-            {
-                ToyTP(other, toy2Area);
-            }
-            if(ToyNumber == 2)
-            {
-                ToyTP(other, toy3Area);
-            }
-            if(ToyNumber == 3)
-            {
-                ToyTP(other, toy4Area);
+                    interaciton.InteractionClear();
+                    ToyNumber++;
+                    break;
+                case 2:
+                    ToyTP(other, toy2Area);
+
+                    interaciton.InteractionClear();
+                    ToyNumber++;
+                    break;
+                case 3:
+                    ToyTP(other, toy3Area);
+
+                    interaciton.InteractionClear();
+                    ToyNumber++;
+                    break;
+                case 4:
+                    ToyTP(other, toy4Area);
+
+                    interaciton.InteractionClear();
+                    ToyNumber++;
+                    break;
+                case 5:
+                    ToyTP(other, toy5Area);
+
+                    interaciton.InteractionClear();
+                    ToyNumber++;
+                    break;
+                case 6:
+                    ToyTP(other, toy6Area);
+
+                    interaciton.InteractionClear();
+                    ToyNumber++;
+                    break;
+                case 7:
+                    ToyTP(other, toy7Area);
+
+                    interaciton.InteractionClear();
+                    ToyNumber++;
+                    break;
+                case 8:
+                    ToyTP(other, toy8Area);
+
+                    interaciton.InteractionClear();
+                    ToyNumber++;
+                    break;
+
             }
 
             gameManager.inCaseToy += 1;
@@ -66,11 +116,14 @@ public class ToyCollect : MonoBehaviour
         }
     }
 
-    public void AreaCreator(GameObject toy , Transform toyArea)
+    public Transform AreaCreator(GameObject toy,string toyAreaName)
     {
-        toy = GameObject.Find($"{toyArea.name}");
-        toyArea = toy.transform;
+        
+        toy = GameObject.Find(toyAreaName);
         toy.SetActive(false);
+
+        return toy.transform;
+        
     }
 
     public void ToyTP(Collider other,Transform toyArea)
