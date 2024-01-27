74379a0d72408af167378d4a6363ac9657af1eb4
SemihGuner <semihgs_890@hotmail.com>
2024-01-27 16:44:55 +0300

    SS e güncelleme.

--- BabyGame/Assets/Script/VoiceLinesController.cs
+++ BabyGame/Assets/Script/VoiceLinesController.cs
@@ -11,9 +11,16 @@ public class VoiceLinesController : MonoBehaviour
 
     // Start is called before the first frame update
     void Start() 
-    { 
-        audioSource = GetComponent<AudioSource>();
-        happiness = GameObject.Find("GameManager").GetComponent<Happiness>();
+    {
+        try
+        {
+            audioSource = GetComponent<AudioSource>();
+            happiness = GameObject.Find("GameManager").GetComponent<Happiness>(); 
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("VoiceLinesController()'da kritik hata: "+e.Message);
+        }
     }
     
     void FixedUpdate()
@@ -27,31 +34,38 @@ public class VoiceLinesController : MonoBehaviour
     }
 
     private void PlayPlayerVoices()
-    {  
-        if (gameJustStarted)
+    {
+        try
         {
-            audioSource.PlayOneShot(voiceLines[0]); // here we go again.
-            gameJustStarted = false;
-        }
-        else if (happiness.getHappiness() >= 75f)
-        { 
-            audioSource.PlayOneShot(voiceLines[1]); // aint half bad
-        }
-        else if (happiness.getHappiness() <= 30f)
-        { 
-            System.Random random = new System.Random();
-            int randomIndex = random.Next(1,3);
-            if (randomIndex == 1)
+            if (gameJustStarted)
             {
-                audioSource.PlayOneShot(voiceLines[6]); // shut up
+                audioSource.PlayOneShot(voiceLines[0]); // here we go again.
+                gameJustStarted = false;
             }
-            else
+            else if (happiness.getHappiness() >= 75f)
             {
-                audioSource.PlayOneShot(voiceLines[4]); // luck you
-            } 
+                audioSource.PlayOneShot(voiceLines[1]); // aint half bad
+            }
+            else if (happiness.getHappiness() <= 30f)
+            {
+                System.Random random = new System.Random();
+                int randomIndex = random.Next(1, 3);
+                if (randomIndex == 1)
+                {
+                    audioSource.PlayOneShot(voiceLines[6]); // shut up
+                }
+                else
+                {
+                    audioSource.PlayOneShot(voiceLines[4]); // luck you
+                }
+            }
+            //TODO: Else if oyun biterse, bitiþ sesini oynat , 2 ve 3.
+            //TODO: Else if kazanýrsak, kazanma sesi, 7.
+            //TODO: Çikolata sesi, 5. 
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("VoiceLinesController()'da kritik hata: " + e.Message);
         } 
-        //TODO: Else if oyun biterse, bitiþ sesini oynat , 2 ve 3.
-        //TODO: Else if kazanýrsak, kazanma sesi, 7.
-        //TODO: Çikolata sesi, 5. 
     }
 }
