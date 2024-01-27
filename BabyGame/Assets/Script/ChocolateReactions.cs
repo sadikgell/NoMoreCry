008b940cc154a1ccaf8bee485599f0863609ebc9
SemihGuner <semihgs_890@hotmail.com>
2024-01-28 01:28:22 +0300

    şokolatlı brokoli bitti

--- BabyGame/Assets/Script/ChocolateReactions.cs
+++ BabyGame/Assets/Script/ChocolateReactions.cs
@@ -6,18 +6,39 @@ public class ChocolateReactions : MonoBehaviour
 {
     [SerializeField] private int remaining = 3;
     [SerializeField] private GameObject chocolateItself;
+    private Interaction interaction;
+    public GameObject ChocolateBrocoli;
     void Start()
     {
         chocolateItself = this.gameObject;
+        interaction = GameObject.Find("Main Camera").GetComponent<Interaction>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("InteractNeg"))
         {
-            other.gameObject.tag = "InteractPos";
-            // TODO: Öteki nesneye çikolata grafiði eklenecek.
-            remaining--;
+            try
+            { 
+                if (other.gameObject.GetComponent<PrefabAccess>().prefabName == "Brocoli")
+                { 
+                    other.gameObject.tag = "InteractPos";
+
+                    GameObject newObject = Instantiate(ChocolateBrocoli, transform.position, transform.rotation);
+                     
+                    Destroy(other.gameObject);
+
+                    newObject.transform.SetParent(GameObject.Find("Food").transform,true);
+                    interaction.InteractionClear();
+
+                    remaining--;
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(e.Message);
+                throw;
+            }
         }
     } 
 
---
+++ BabyGame/Assets/Script/PrefabAccess.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabAccess : MonoBehaviour
+{
+    // Variable to store the prefab name
+    public string prefabName;
+
+    // SetPrefabName method to set the prefab name
+    public void SetPrefabName(string name)
+    {
+        prefabName = name;
+    }
+}
